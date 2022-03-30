273778343ec7eb8f8ca129f49454e81e87bb74b7
escabora <gabrielescabora@gmail.com>
2022-03-30 00:45:17 -0300

    feat: link menu

--- app/src/App.js
+++ app/src/App.js
@@ -9,11 +9,11 @@ function App() {
         <div class='content'>
           <img src={logo} className='App-logo' alt='logo' />
           <nav>
-            <a href=''>Link</a>
-            <a href=''>Link</a>
-            <a href=''>Link</a>
-            <a href=''>Link</a>
-            <a href=''>Link</a>
+            <a href=''>Link 1</a>
+            <a href=''>Link 2</a>
+            <a href=''>Link 3</a>
+            <a href=''>Link 4</a>
+            <a href=''>Link 5</a>
           </nav>
         </div>
       </header>
