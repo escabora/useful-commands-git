e3b545252657f061690f6975ff57181bfcfe95e2
escabora <gabrielescabora@gmail.com>
2022-04-01 14:06:43 -0300

    links

--- app/src/App.js
+++ app/src/App.js
@@ -7,25 +7,43 @@ function App() {
   useEffect(() => {
     console.log('aqui no render');
   }, []);
+  const handleClick = (ev) => {
+    ev.preventDefault();
+    console.log(ev.target.innerHTML);
+  };
   return (
     <div className='App'>
       <header className='App-header'>
-        <div class='content'>
+        <div className='content'>
           <img src={logo} className='App-logo logo-inner' alt='logo' />
           <nav>
-            <a href=''>Link 1</a>
-            <a href=''>Link 2</a>
-            <a href=''>Link 3</a>
-            <a href=''>Link 4</a>
-            <a href=''>Link 5</a>
-            <a href=''>Link 6</a>
-            <a href=''>Link 7</a>
+            <a href='' onClick={(ev) => handleClick(ev)}>
+              Link 1
+            </a>
+            <a href='' onClick={(ev) => handleClick(ev)}>
+              Link 2
+            </a>
+            <a href='' onClick={(ev) => handleClick(ev)}>
+              Link 3
+            </a>
+            <a href='' onClick={(ev) => handleClick(ev)}>
+              Link 4
+            </a>
+            <a href='' onClick={(ev) => handleClick(ev)}>
+              Link 5
+            </a>
+            <a href='' onClick={(ev) => handleClick(ev)}>
+              Link 6
+            </a>
+            <a href='' onClick={(ev) => handleClick(ev)}>
+              Link 7
+            </a>
           </nav>
         </div>
       </header>
-      <div class='content-cards'>
+      <div className='content-cards'>
         {mock.map((item, index) => (
-          <div key={index} class='card'>
+          <div key={index} className='card'>
             <p>{item.text}</p>
           </div>
         ))}
